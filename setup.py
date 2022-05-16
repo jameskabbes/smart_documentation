090e34e5f86c62f157d40b9bec07c0eb6f94be24
James Kabbes <james.kabbes@gmail.com>
2022-05-16 18:37:27 -0500

    prepping for build

--- setup.py
+++ setup.py
@@ -4,7 +4,11 @@ if __name__ == '__main__':
     setup(
         package_data={'smart_documentation': 
         [ 
-            'Templates/default/Template/docs/*.html',
+            'Templates/default/Template/docs/_static/*.css',
+            'Templates/default/Template/docs/_templates/*.rst',
+            'Templates/default/Template/docs/*.py',
+            'Templates/default/Template/docs/*.rst',
+            'Templates/github_workflows_yml/Template/.github/workflows/*.yml',
         ]
         }
     )
