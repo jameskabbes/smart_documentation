e25a12241d2f2b9806db0df28d0abfbd3874f9f2
James Kabbes <james.kabbes@gmail.com>
2023-03-23 19:37:07 -0500

    defining new yml pattern

--- src/kabbes_smart_documentation/Client.py
+++ src/kabbes_smart_documentation/Client.py
@@ -1,6 +1,6 @@
 import kabbes_smart_documentation
 import kabbes_client
-import pypi_builder
+import kabbes_pypi_builder
 
 class Client( kabbes_smart_documentation.DocumentationGenerator ):
 
@@ -14,7 +14,7 @@ class Client( kabbes_smart_documentation.DocumentationGenerator ):
 
         self.Package = kabbes_client.Package( kabbes_smart_documentation._Dir, dict=d )
         cfg_sd = self.Package.cfg
-        cfg_pypi = pypi_builder.Client().cfg
+        cfg_pypi = kabbes_pypi_builder.Client().cfg
 
         cfg_pypi.merge( cfg_sd )
         self.cfg = cfg_pypi
--- src/kabbes_smart_documentation/DocumentationGenerator.py
+++ src/kabbes_smart_documentation/DocumentationGenerator.py
@@ -1,10 +1,10 @@
-import pypi_builder
+import kabbes_pypi_builder
 import os
 
-class DocumentationGenerator( pypi_builder.PackageGenerator ):
+class DocumentationGenerator( kabbes_pypi_builder.PackageGenerator ):
 
     def __init__( self ):
-        pypi_builder.PackageGenerator.__init__( self )
+        kabbes_pypi_builder.PackageGenerator.__init__( self )
 
     def generate_sphinx( self ):
         os.system( self.cfg['sphinx_script_Path'].read() ) 
